0be3acd3666d309a2edc186c84c44de2aa954cc1
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-23 04:05:03 +0530

    Update play.py

--- Pronova/Plugins/Plays/play.py
+++ Pronova/Plugins/Plays/play.py
@@ -1,4 +1,6 @@
 from pyrogram import filters
+from traceback import format_exc
+
 from Pronova.Bot import bot, engine
 from Pronova.Utils.Assistant import get_ass
 from Pronova.Utils.Font import sc
@@ -10,6 +12,8 @@ from Pronova.Database.Users import add_user
 from Pronova.Database.Chats import add_chat
 from Pronova.Database.Auth import is_auth
 
+from Pronova.Utils.Logger import LOGGER
+
 
 async def check_ban(m):
     if not m.from_user:
@@ -19,10 +23,12 @@ async def check_ban(m):
     chat_id = m.chat.id
 
     if await is_gbanned(uid):
+        LOGGER.warning(f"[GBANNED USER] {uid}")
         await m.reply(sc("you are gbanned"))
         return True
 
     if await is_banned(chat_id, uid):
+        LOGGER.warning(f"[BANNED IN CHAT] {uid} in {chat_id}")
         await m.reply(sc("you are banned in this chat"))
         return True
 
@@ -32,7 +38,7 @@ async def check_ban(m):
 async def safe_delete(m):
     try:
         await m.delete()
-    except:
+    except Exception:
         pass
 
 
@@ -43,8 +49,8 @@ async def register_usage(m):
     try:
         await add_user(m.from_user)
         await add_chat(m.chat)
-    except Exception as e:
-        print("Usage Register Error:", e)
+    except Exception:
+        LOGGER.error(f"[USAGE REGISTER ERROR]\n{format_exc()}")
 
 
 async def handle_play(m, force=False, video=False):
@@ -55,34 +61,48 @@ async def handle_play(m, force=False, video=False):
     uid = m.from_user.id
     chat_id = m.chat.id
 
+    LOGGER.info(f"[PLAY CMD] User: {uid} | Chat: {chat_id} | Force: {force} | Video: {video}")
+
     if await check_ban(m):
         return
 
     if force:
-        member = await bot.get_chat_member(chat_id, uid)
-        if member.status not in ("administrator", "creator"):
-            return await m.reply(sc("admins only"))
+        try:
+            member = await bot.get_chat_member(chat_id, uid)
+            if member.status not in ("administrator", "creator"):
+                LOGGER.warning(f"[FORCE DENIED] {uid} not admin in {chat_id}")
+                return await m.reply(sc("admins only"))
+        except Exception:
+            LOGGER.error(f"[ADMIN CHECK ERROR]\n{format_exc()}")
+            return
     else:
         if not await is_auth(chat_id, uid):
             if not await is_allowed(bot, m, notify=True):
+                LOGGER.warning(f"[NOT ALLOWED] {uid} in {chat_id}")
                 return
 
     if not await get_ass(chat_id, m):
+        LOGGER.warning(f"[ASSISTANT JOIN FAILED] Chat: {chat_id}")
         return
 
     if force:
         try:
+            LOGGER.info(f"[FORCE STOP] Chat: {chat_id}")
             await engine.vc.stop(chat_id)
-        except:
-            pass
+        except Exception:
+            LOGGER.error(f"[FORCE STOP ERROR]\n{format_exc()}")
 
     reply = m.reply_to_message
 
     if reply and (reply.voice or reply.audio or reply.video):
 
+        LOGGER.info(f"[MEDIA PLAY] Chat: {chat_id}")
+
         try:
             path = await reply.download()
-        except:
+            LOGGER.debug(f"[DOWNLOADED FILE] {path}")
+        except Exception:
+            LOGGER.error(f"[DOWNLOAD FAILED]\n{format_exc()}")
             return await m.reply(sc("download failed"))
 
         try:
@@ -93,19 +113,25 @@ async def handle_play(m, force=False, video=False):
                 reply=reply,
                 video=video
             )
-        except:
+        except Exception:
+            LOGGER.error(f"[PLAY FILE ERROR]\n{format_exc()}")
             return await m.reply(sc("unable to play media"))
 
         if not song:
+            LOGGER.warning("[PLAY FILE FAILED]")
             return await m.reply(sc("unable to play media"))
 
         await inc_song_play(chat_id, title)
+        LOGGER.info(f"[MEDIA PLAY SUCCESS] {title}")
         return
 
     if len(m.command) < 2:
+        LOGGER.warning("[NO QUERY PROVIDED]")
         return await m.reply(sc("give song name"))
 
     query = m.text.split(None, 1)[1]
+
+    LOGGER.info(f"[SEARCH QUERY] {query}")
 
     try:
         song, title = await engine.vc.play(
@@ -114,13 +140,16 @@ async def handle_play(m, force=False, video=False):
             m.from_user.mention,
             video=video
         )
-    except:
+    except Exception:
+        LOGGER.error(f"[PLAY ERROR]\n{format_exc()}")
         return await m.reply(sc("unable to play song"))
 
     if not song:
+        LOGGER.warning(f"[PLAY FAILED] Query: {query}")
         return await m.reply(sc("unable to play song"))
 
     await inc_song_play(chat_id, title or query)
+    LOGGER.info(f"[PLAY SUCCESS] {title or query}")
 
 
 @bot.on_message(filters.command(["play"]) & filters.group)
