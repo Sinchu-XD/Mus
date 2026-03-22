12db8a9f3749f1b055ea57b43e0d1992f83e1c71
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-22 13:19:02 +0530

    Update Handling.py

--- Pronova/Player/Handling.py
+++ Pronova/Player/Handling.py
@@ -1,17 +1,31 @@
+from traceback import format_exc
+
 from pytgcalls import filters
 from pytgcalls.types import StreamEnded, ChatUpdate
 
 
-def setup_handlers(core, on_end=None, on_vc_closed=None):
+def setup_handlers(core, plugin=None, on_end=None, on_vc_closed=None):
 
     @core.on_update(filters.stream_end())
     async def stream_end(_, update: StreamEnded):
-        if on_end:
-            await on_end(update.chat_id)
+        try:
+            if plugin:
+                await plugin.on_song_end(update.chat_id, None)
+
+            if on_end:
+                await on_end(update.chat_id)
+        except Exception:
+            print(format_exc())
 
     @core.on_update(
         filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT)
     )
     async def vc_closed(_, update: ChatUpdate):
-        if on_vc_closed:
-            await on_vc_closed(update.chat_id)
+        try:
+            if plugin:
+                await plugin.on_vc_closed(update.chat_id)
+
+            if on_vc_closed:
+                await on_vc_closed(update.chat_id)
+        except Exception:
+            print(format_exc())
