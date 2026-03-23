aca4ed11254f314a765ad8bc3abe0f73c9798b3a
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-23 12:11:18 +0530

    Create Allow.py

---
+++ Pronova/Utils/Allow.py
@@ -0,0 +1,75 @@
+from pyrogram.enums import ChatMemberStatus
+from Pronova.Database import is_sudo, is_auth, is_banned, is_gbanned
+from Pronova.Utils.Font import sc
+from Config import OWNER_ID
+
+
+async def get_user_data(message=None, cq=None):
+    if message:
+        return message.from_user, message.chat.id
+    if cq:
+        return cq.from_user, cq.message.chat.id
+    return None, None
+
+
+async def is_admin(client, chat_id, user_id):
+    try:
+        member = await client.get_chat_member(chat_id, user_id)
+        return member.status in (
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER
+        )
+    except:
+        return False
+
+
+async def owner_only(client, message=None, cq=None):
+    user, _ = await get_user_data(message, cq)
+
+    if not user:
+        return False
+
+    return user.id == OWNER_ID
+
+
+async def sudo_only(client, message=None, cq=None):
+    user, _ = await get_user_data(message, cq)
+
+    if not user:
+        return False
+
+    uid = user.id
+
+    if uid == OWNER_ID:
+        return True
+
+    return await is_sudo(uid)
+
+
+async def admin_only(client, message=None, cq=None):
+    user, chat_id = await get_user_data(message, cq)
+
+    if not user:
+        return False
+
+    uid = user.id
+
+    if await is_gbanned(uid):
+        return False
+
+    if await is_banned(chat_id, uid):
+        return False
+
+    if uid == OWNER_ID:
+        return True
+
+    if await is_sudo(uid):
+        return True
+
+    if await is_admin(client, chat_id, uid):
+        return True
+
+    if await is_auth(chat_id, uid):
+        return True
+
+    return False
