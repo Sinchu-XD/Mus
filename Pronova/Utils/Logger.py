7064dffe0110599b087b235dba87f84f43391bbc
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-03-26 02:50:01 +0530

    Update Logger.py

--- Pronova/Utils/Logger.py
+++ Pronova/Utils/Logger.py
@@ -10,7 +10,7 @@ os.makedirs("logs", exist_ok=True)
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] - %(name)s - %(message)s"
 
 logging.basicConfig(
-    level=logging.ERROR, 
+    level=logging.INFO, 
     format=LOG_FORMAT,
     handlers=[
         logging.StreamHandler(sys.stdout),
@@ -23,11 +23,11 @@ logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 
 LOGGER = logging.getLogger("Pronova")
-LOGGER.setLevel(logging.ERROR)
+LOGGER.setLevel(logging.INFO)
 
 
 def set_debug(enabled: bool):
     if enabled:
         LOGGER.setLevel(logging.DEBUG)
     else:
-        LOGGER.setLevel(logging.ERROR) 
+        LOGGER.setLevel(logging.INFO) 
